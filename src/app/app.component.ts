3b3df3f23ac731f48808ed5108e623319b6d7d51
imetov <imetov84@gmail.com>
2020-04-09 17:41:52 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,29 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit{
   name = 'Angular Google';
 
-  columnDefs = [
+  /*columnDefs = [
     { headerName: 'Make', field: 'make', sortable: true, filter: true },
     { headerName: 'Model', field: 'model', sortable: true, filter: true },
     { headerName: 'Price', field: 'price', sortable: true, filter: true }];
 
-  /*rowData = [
+  rowData = [
     { make: 'Toyota', model: 'Celica', price: 35000 },
     { make: 'Ford', model: 'Mondeo', price: 32000 },
     { make: 'Porsche', model: 'Boxter', price: 72000 }
   ];*/
+  columnDefs = [
+    { headerName: 'Author', field: 'commit.author.name', sortable: true, filter: true },
+    { headerName: 'Email', field: 'commit.author.email', sortable: true, filter: true },
+    { headerName: 'login', field: 'author.login', sortable: true, filter: true }];
+
   rowData: any;
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
-    this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
+    this.rowData = this.http.get('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits');
+    //https://api.myjson.com/bins/15psn9
+    //https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits
   }
 }
